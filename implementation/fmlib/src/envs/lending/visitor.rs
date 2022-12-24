90453574066b55bb12c0fdb1cd9e9782200f6929
moyarka <mahyar.karimi2079@gmail.com>
2022-12-24 20:15:45 +0330

    remove visitor.rs

--- implementation/fmlib/src/envs/lending/visitor.rs
+++
@@ -1,8 +0,0 @@
-use crate::envs::lending::lv::*;
-
-pub trait Visitor {
-    fn visit_env(&mut self, v: &EnvVertex);
-    fn visit_sample(&mut self, v: &SampleVertex);
-    fn visit_decision(&mut self, v: &DecisionVertex);
-    fn visit_payback(&mut self, v: &PaybackVertex);
-}
