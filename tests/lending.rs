ffb5fc5ef0f9b76c03622c7d90d4c2c19ca805de
mahykari <mahyar.karimi2079@gmail.com>
2023-06-07 16:00:33 +0200

    update tests/lending.rs

--- tests/lending.rs
+++ tests/lending.rs
@@ -1,43 +1,22 @@
 #[cfg(test)]
 mod tests {
-    use std::collections::HashMap;
-    use std::vec;
-    use fmlib::envs::lending::{
-        lending::*,
-        markov_chain::*,
-    };
+  use fmlib::util;
 
-    #[test]
-    fn build_markov_chain_memless() {
-        let payback_prob = (0..10).map(
-            |x| (x, ((x + 5) % 10) as f64 / 10.0)
-        ).collect::<HashMap<_, _>>();
-        
-        let mut policy = HashMap::<(i32, i32), f64>::new();
-        (0..10).for_each(|i| {
-            policy.insert((0, i), (i + 1) as f64 / 10.0);
-        });
-        (0..10).for_each(|i| {
-            policy.insert((1, i), ((i + 8) % 10) as f64 / 10.0);
-        });
+  #[test]
+  fn test_lending_small() {
+    let mc = util::markov_chain_lending_small();
 
-        let init_credit = vec![
-            vec![2, 2, 2, 2, 2, 2, 2, 2, 2, 2],
-            vec![5, 5, 1, 1, 1, 1, 1, 2, 2, 1]
-        ];
-
-        let lending = Lending {
-            group_count: 2,
-            credit_score: 10,
-            group_population: vec![20, 20],
-            payback_prob,
-            init_credit,
-            policy,
-        };
-        
-        let mc = LendingMarkovChain::new(lending);
-        for (i, s) in mc.take(100).enumerate() {
-            println!("{}: {:?}", i, s);
-        }
+    for v in mc.take(10) {
+      println!("{:?}", v);
     }
+  }
+
+  #[test]
+  fn test_lending_large() {
+    let mc = util::markov_chain_lending_large();
+
+    for v in mc.take(10) {
+      println!("{:?}", v);
+    }
+  }
 }
